92cd3fbb428f122f4130186ff4af4025ef3b3cd7
vczh <vczh@163.com>
2015-04-18 01:31:56 -0700

    ...

--- Tools/BuildTools.bat
+++ Tools/BuildTools.bat
@@ -1,5 +1,6 @@
 mkdir .Output
 
+MSBUILD Codepack\Codepack.sln                                   /p:Configuration=Release;OutputPath=..\..\.Output\
 MSBuild ..\..\Vlpp\Tools\ParserGen\ParserGen\ParserGen.vcxproj	/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\
 MSBuild ..\..\GacUI\Tools\GacGen\GacGen\GacGen.vcxproj		/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\
 
