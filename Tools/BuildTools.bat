011e75f190cfc54f9f483a859ba3d3e6a2bb28ca
vczh <vczh@163.com>
2015-08-25 00:50:48 -0700

    Move DocTools from GacUI to Tools

--- Tools/BuildTools.bat
+++ Tools/BuildTools.bat
@@ -1,6 +1,7 @@
 mkdir .Output
 
 MSBUILD Codepack\Codepack.sln                                   /p:Configuration=Release;OutputPath=..\..\.Output\
+MSBUILD DocTools\DocTools.sln                                   /p:Configuration=Release;OutputPath=..\..\.Output\
 MSBuild ..\..\Vlpp\Tools\ParserGen\ParserGen\ParserGen.vcxproj	/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\
 MSBuild ..\..\GacUI\Tools\GacGen\GacGen\GacGen.vcxproj		/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\
 
