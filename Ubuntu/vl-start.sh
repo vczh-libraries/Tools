e305ded22a9e29018f0e251e62317b6788497af3
Zihan Chen <vczh@163.com>
2016-12-03 22:16:59 -0800

    Update vl-start.sh

--- Ubuntu/vl-start.sh
+++ Ubuntu/vl-start.sh
@@ -3,10 +3,19 @@
 cd vl
 export VPATH_CONTROL=${PWD}/vl
 export PATH=${VPATH_CONTROL}:${PATH}
-if ! [ -a vl-ssh.sh]; then curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-ssh.sh
-if ! [ -a vl-apt.sh]; then curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-apt.sh
-if ! [ -a vl-enlist.sh]; then curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-enlist.sh
-chmod u+x vl-*.sh
+
+if ! [ -a vl-ssh.sh]; then
+    curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-ssh.sh
+    chmod u+x vl-ssh.sh
+fi
+if ! [ -a vl-apt.sh]; then
+    curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-apt.sh
+    chmod u+x vl-apt.sh
+fi
+if ! [ -a vl-enlist.sh]; then
+    curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-enlist.sh
+    chmod u+x vl-enlist.sh
+fi
 
 cd ..
 echo Welcome to Vczh Libraries Control Panel!
