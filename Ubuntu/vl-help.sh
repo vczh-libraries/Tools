52bca0f39e352978ee267ac1b1262b150ec0193d
Zihan Chen <vczh@163.com>
2016-12-03 22:33:10 -0800

    Create vl-help.sh

---
+++ Ubuntu/vl-help.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "vl-help"
+echo "    Help information for Vczh Libraries Control Panel."
+echo "vl-ssh.sh --help"
+echo "    Quick way to setup ssh key for your github account."
+echo "vl-apt.sh --help"
+echo "    Download necessary softwares."
+echo "vl-enlist.sh --help"
+echo "    Enlist all repos from github."
