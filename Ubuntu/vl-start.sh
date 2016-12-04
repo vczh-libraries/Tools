95f49d07981c37ac3f9b67341d1c0e068c79fa7c
Zihan Chen <vczh@163.com>
2016-12-03 22:20:45 -0800

    Update vl-start.sh

--- Ubuntu/vl-start.sh
+++ Ubuntu/vl-start.sh
@@ -4,15 +4,15 @@ cd vl
 export VPATH_CONTROL=${PWD}
 export PATH=${VPATH_CONTROL}:${PATH}
 
-if ! [ -a vl-ssh.sh]; then
+if ! [ -a vl-ssh.sh ]; then
     curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-ssh.sh
     chmod u+x vl-ssh.sh
 fi
-if ! [ -a vl-apt.sh]; then
+if ! [ -a vl-apt.sh ]; then
     curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-apt.sh
     chmod u+x vl-apt.sh
 fi
-if ! [ -a vl-enlist.sh]; then
+if ! [ -a vl-enlist.sh ]; then
     curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-enlist.sh
     chmod u+x vl-enlist.sh
 fi
