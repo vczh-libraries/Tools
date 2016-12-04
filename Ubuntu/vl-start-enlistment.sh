d7d9810a6416cb126959149f867d09bf140df854
Zihan Chen <vczh@163.com>
2016-12-04 02:03:08 -0800

    Update vl-start-enlistment.sh

--- Ubuntu/vl-start-enlistment.sh
+++ Ubuntu/vl-start-enlistment.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "vl-start-enlistment.sh is under development"
+export PATH=${PWD}/vl/Enlistment:${PATH}
+
+echo "Welcome to Vczh Libraries Control Panel!"
+echo "Use help for help information."
