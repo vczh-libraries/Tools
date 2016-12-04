8b0330d25ec8d9a11352446ca0179aa2aa166571
Zihan Chen <vczh@163.com>
2016-12-03 21:34:30 -0800

    Update vl-start.sh

--- Ubuntu/vl-start.sh
+++ Ubuntu/vl-start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+cd vl
+export VPATH_CONTROL=$(PWD)/vl
+export PATH=$(VPATH_CONTROL):$(PATH)
+curl -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-ssh.sh
+curl -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-apt.sh
+curl -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/vl-enlist.sh
+chmod u+x vl-*.sh
+cd ..
+
 echo Welcome to Vczh Libraries Control Panel!
