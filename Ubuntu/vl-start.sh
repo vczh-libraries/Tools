26bbb0a9ab7f62d27d017e323b9dd21ee74b5bac
Zihan Chen <vczh@163.com>
2016-12-04 00:10:42 -0800

    Update vl-start.sh

--- Ubuntu/vl-start.sh
+++ Ubuntu/vl-start.sh
@@ -4,17 +4,17 @@ cd vl
 export VPATH_CONTROL=${PWD}
 export PATH=${VPATH_CONTROL}:${PATH}
 
-function get-script {
+function GetScript {
     if ! [ -a $1 ]; then
         curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/vczh-libraries/Tools/master/Ubuntu/$1
         chmod u+x $1
     fi
 }
-get-script vl-help.sh
-get-script vl-ssh.sh
-get-script vl-apt.sh
-get-script vl-enlist.sh
-unset -f get-script
+GetScript vl-help.sh
+GetScript vl-ssh.sh
+GetScript vl-apt.sh
+GetScript vl-enlist.sh
+unset -f GetScript
 
 cd ..
 echo "Welcome to Vczh Libraries Control Panel!"
