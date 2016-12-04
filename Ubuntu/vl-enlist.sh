b804b2cffb46d39b6bcabcb00dcbe40cde05d660
Zihan Chen <vczh@163.com>
2016-12-04 00:20:12 -0800

    Update vl-enlist.sh

--- Ubuntu/vl-enlist.sh
+++ Ubuntu/vl-enlist.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-echo vl-enlist.sh is under development.
+function Help {
+    echo "Usage:"
+    echo "--enlist"
+    echo "    Enlist all repos in the current directory."
+}
+
+function GitClone {
+    git clone git@github.com:vczh-libraries/${1}.git
+}
+
+function Enlist {
+    if [ ${PWD} == ~/Desktop ]; then
+        echo "I don't think you want to enlist repos in your desktop folder."
+        exit 1
+    fi
+    GitClone Tools
+    GitClone Vlpp
+    GitClone Workflow
+    GitClone GacUI
+    GitClone GacJS
+    GitClone Release
+    GitClone XGac
+    GitClone iGac
+}
+
+case $1 in
+    --help)
+    Help
+    ;;
+
+    --enlist)
+    Enlist
+    ;;
+
+    *)
+    echo "Use --help for more information."
+    ;;
+esac
