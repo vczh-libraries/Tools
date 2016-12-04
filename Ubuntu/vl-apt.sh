1241b310ad859155cd7a2ef69ba05b2d8f681dc5
Zihan Chen <vczh@163.com>
2016-12-03 23:42:43 -0800

    Update vl-apt.sh

--- Ubuntu/vl-apt.sh
+++ Ubuntu/vl-apt.sh
@@ -16,30 +16,30 @@ function Help {
     echo "    Remove all required softwares"
 }
 
-function AptGet-Install {
+function AptGetInstall {
     echo "Installing $1 ..."
     sudo apt-get install "$1" --assume-yes > /dev/null
 }
 
-function AptGet-Remove {
+function AptGetRemove {
     echo "Removing $1 ..."
     sudo apt-get remove "$1" --assume-yes > /dev/null
 }
 
-function AptGet-AutoRemove {
+function AptGetAutoRemove {
     echo "Removing unnecessary dependencies ..."
     sudo apt-get autoremove --assume-yes > /dev/null
 }
 
 function Install {
-    AptGet-Install htop
-    AptGet-Install cgdb
-    AptGet-Install manpages-posix-dev
-    AptGet-Install git
-    AptGet-Install apt-file
-    AptGet-Install clang
-    AptGet-Install openssh-server
-    AptGet-Install lconv
+    AptGetInstall htop
+    AptGetInstall cgdb
+    AptGetInstall manpages-posix-dev
+    AptGetInstall git
+    AptGetInstall apt-file
+    AptGetInstall clang
+    AptGetInstall openssh-server
+    AptGetInstall lconv
     
     echo "Starting ssh service ..."
     /etc/init.d/ssh start
@@ -49,16 +49,16 @@ function Remove {
     echo "Stopping ssh service ..."
     /etc/init.d/ssh stop
 
-    AptGet-Remove htop
-    AptGet-Remove cgdb
-    AptGet-Remove manpages-posix-dev
-    AptGet-Remove git
-    AptGet-Remove apt-file
-    AptGet-Remove clang
-    AptGet-Remove openssh-server
-    AptGet-Remove lconv
+    AptGetRemove htop
+    AptGetRemove cgdb
+    AptGetRemove manpages-posix-dev
+    AptGetRemove git
+    AptGetRemove apt-file
+    AptGetRemove clang
+    AptGetRemove openssh-server
+    AptGetRemove lconv
 
-    AptGet-AutoRemove
+    AptGetAutoRemove
 }
 
 case $1 in
