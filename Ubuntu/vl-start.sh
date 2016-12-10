4b9e889d40a7752a794200ef12f96e44654cbb20
Zihan Chen <vczh@163.com>
2016-12-09 21:44:27 -0800

    Update vl-start.sh

--- Ubuntu/vl-start.sh
+++ Ubuntu/vl-start.sh
@@ -40,7 +40,7 @@ function GetScript {
 cd ./vl
 
     GetScript ControlPanel vl help ssh apt enlist
-    GetScript Enlistment vle help go util_CppFromVcxproj util_CppDependencies
+    GetScript Enlistment vle help go t4 util_CppFromVcxproj util_CppDependencies
 
     GetFile vl-start-enlistment.sh
     chmod u+x vl-start-enlistment.sh
