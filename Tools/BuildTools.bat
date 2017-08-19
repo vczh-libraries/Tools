abf3295ff8a0d42f73118166e42575f477fc3bac
Zihan Chen <vczh@163.com>
2017-08-19 15:26:31 -0700

    Update CodePack

--- Tools/BuildTools.bat
+++ Tools/BuildTools.bat
@@ -1,9 +1,9 @@
 mkdir .Output
 
-MSBUILD Codepack\Codepack.sln                                   /p:Configuration=Release;OutputPath=..\..\.Output\
-MSBUILD DocTools\DocTools.sln                                   /p:Configuration=Release;OutputPath=..\..\.Output\
+MSBUILD DocTools\DocTools.sln									/p:Configuration=Release;OutputPath=..\..\.Output\
+MSBuild ..\..\Vlpp\Tools\CodePack\CodePack\CodePack.vcxproj		/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\
 MSBuild ..\..\Vlpp\Tools\ParserGen\ParserGen\ParserGen.vcxproj	/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\
-MSBuild ..\..\GacUI\Tools\GacGen\GacGen\GacGen.vcxproj		/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\
+MSBuild ..\..\GacUI\Tools\GacGen\GacGen\GacGen.vcxproj			/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\
 
 copy .Output\*.exe *.exe
 copy .Output\*.dll *.dll
