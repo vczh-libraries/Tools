dbfab758e26be73bcccb84841798767c92610f48
Zihan Chen <vczh@163.com>
2017-08-25 06:30:40 -0700

    GacGen.bat

--- Tools/BuildTools.bat
+++ Tools/BuildTools.bat
@@ -4,14 +4,14 @@ del *.wait.tmp
 del *.exe
 del *.dll
 
-start RunAndLock %RANDOM% "MSBUILD DocTools\DocTools.sln									/p:Configuration=Release;OutputPath=..\..\.Output\"
-start RunAndLock %RANDOM% "MSBuild ..\..\Vlpp\Tools\CodePack\CodePack\CodePack.vcxproj		/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\"
-start RunAndLock %RANDOM% "MSBuild ..\..\Vlpp\Tools\ParserGen\ParserGen\ParserGen.vcxproj	/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\"
-start RunAndLock %RANDOM% "MSBuild ..\..\Workflow\Tools\CppMerge\CppMerge\CppMerge.vcxproj	/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\"
-start RunAndLock %RANDOM% "MSBuild ..\..\GacUI\Tools\GacGen\GacGen\GacGen.vcxproj			/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\GacGen(x32)\"
-start RunAndLock %RANDOM% "MSBuild ..\..\GacUI\Tools\GacGen\GacGen\GacGen.vcxproj			/p:Configuration=Release;Platform=x64;OutDir=..\..\..\..\Tools\Tools\.Output\GacGen(x64)\"
+start %~dp0RunAndLock %RANDOM% "MSBUILD DocTools\DocTools.sln									/p:Configuration=Release;OutputPath=..\..\.Output\"
+start %~dp0RunAndLock %RANDOM% "MSBuild ..\..\Vlpp\Tools\CodePack\CodePack\CodePack.vcxproj		/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\"
+start %~dp0RunAndLock %RANDOM% "MSBuild ..\..\Vlpp\Tools\ParserGen\ParserGen\ParserGen.vcxproj	/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\"
+start %~dp0RunAndLock %RANDOM% "MSBuild ..\..\Workflow\Tools\CppMerge\CppMerge\CppMerge.vcxproj	/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\"
+start %~dp0RunAndLock %RANDOM% "MSBuild ..\..\GacUI\Tools\GacGen\GacGen\GacGen.vcxproj			/p:Configuration=Release;Platform=x86;OutDir=..\..\..\..\Tools\Tools\.Output\GacGen(x32)\"
+start %~dp0RunAndLock %RANDOM% "MSBuild ..\..\GacUI\Tools\GacGen\GacGen\GacGen.vcxproj			/p:Configuration=Release;Platform=x64;OutDir=..\..\..\..\Tools\Tools\.Output\GacGen(x64)\"
 
-call WaitForLock
+call %~dp0WaitForLock
 
 copy .Output\*.exe *.exe
 copy .Output\*.dll *.dll
