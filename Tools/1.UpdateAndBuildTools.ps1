d43fe8d33e9a3d94935f5d2d4868767b4866e1da
Zihan Chen <vczh@163.com>
2017-10-09 05:48:16 -0700

    ...

--- Tools/1.UpdateAndBuildTools.ps1
+++ Tools/1.UpdateAndBuildTools.ps1
@@ -87,33 +87,6 @@ function Update-And-Build-Tools {
     Test-Single-Binary-Rename "GacGen(x64)\GacGen.exe" GacGen64.exe
 }
 
-function GacGen($FileName) {
-    Write-Host "Compiling GacUI Resource: $FileName ..."
-    $gacgen_32 = Start-Process "$PSScriptRoot\GacGen32.exe" -ArgumentList "/P $FileName" -PassThru
-    $gacgen_64 = Start-Process "$PSScriptRoot\GacGen64.exe" -ArgumentList "/P $FileName" -PassThru
-    $gacgen_32.WaitForExit()
-    $gacgen_64.WaitForExit()
-
-    if (Test-Path -Path "$($FileName).log\x32\Error.txt") {
-        throw "Failed to compile GacUI Resource (x86): $FileName"
-    }
-    if (Test-Path -Path "$($FileName).log\x64\Error.txt") {
-        throw "Failed to compile GacUI Resource (x64): $FileName"
-    }
-    
-    $output_folder = Get-Content "$($FileName).log\x32\CppOutput.txt"
-    $x32_folder = "$($FileName).log\x32\Source"
-    $x64_folder = "$($FileName).log\x64\Source"
-    if (!(Test-Path -Path $output_folder)) {
-        New-Item $output_folder -ItemType directory | Out-Null
-    }
-    Get-ChildItem -Path $output_folder -ErrorAction SilentlyContinue | %{
-        Write-Host "Merging C++ Source File: $output_folder\$($_.Name) ..."
-        $cppmerge = Start-Process "$PSScriptRoot\CppMerge.exe" -ArgumentList "/P `"$x32_folder\$($_.Name)`" `"$x64_folder\$($_.Name)`" `"$output_folder\$($_.Name)`"" -PassThru
-        $cppmerge.WaitForExit();
-    }
-}
-
 function Update-GacUI-Skins {
     # Update DarkSkin
     Write-Host "Update GacUI::DarkSkin ..."
@@ -121,7 +94,7 @@ function Update-GacUI-Skins {
     try {
         Remove-Item *.xml
         Copy-Item ..\..\..\Test\GacUISrc\Host\Resources\DarkSkin\*.xml .
-        GacGen Resource.xml
+        & $PSScriptRoot\GacGen.ps1 -FileName Resource.xml
     }
     finally {
         Pop-Location
