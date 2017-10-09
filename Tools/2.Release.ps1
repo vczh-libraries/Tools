f042c543034cc548fc0335209e6cb7ba08631cd2
Zihan Chen <vczh@163.com>
2017-10-09 06:34:33 -0700

    ...

---
+++ Tools/2.Release.ps1
@@ -0,0 +1,48 @@
+. $PSScriptRoot\Common.ps1
+
+Push-Location $PSScriptRoot\..\..\Release | Out-Null
+
+try {
+    # Import
+    Write-Host "Copying Source Code ..."
+    New-Item .\Import\Skins -ItemType directory -ErrorAction SilentlyContinue | Out-Null
+    New-Item .\Import\Skins\DarkSkin -ItemType directory -ErrorAction SilentlyContinue | Out-Null
+    Copy-Item ..\Vlpp\Release\*.h .\Import
+    Copy-Item ..\Vlpp\Release\*.cpp .\Import
+    Copy-Item ..\Workflow\Release\*.h .\Import
+    Copy-Item ..\Workflow\Release\*.cpp .\Import
+    Copy-Item ..\GacUI\Release\Gac*.h .\Import
+    Copy-Item ..\GacUI\Release\Gac*.cpp .\Import
+    Copy-Item ..\GacUI\Release\DarkSkin* .\Import\Skins\DarkSkin
+
+    # Deploy
+    Write-Host "Deploying Binaries ..."
+    Copy-Item $PSScriptRoot\CppMerge.exe .\Tools
+    Copy-Item $PSScriptRoot\GacGen32.exe .\Tools
+    Copy-Item $PSScriptRoot\GacGen64.exe .\Tools
+    Copy-Item $PSScriptRoot\GacGen.ps1 .\Tools
+
+    # ControlTemplate\BlackSkin
+    Write-Host "Deploying Tutorial\GacUI_ControlTemplate\BlackSkin ..."
+    Push-Location $PSScriptRoot\..\..\Release\Tutorial\GacUI_ControlTemplate\BlackSkin\UI | Out-Null
+    Remove-Item .\FullControlTest -Force -Recurse | Out-Null
+    Copy-Item $PSScriptRoot\..\..\GacUI\Test\GacUISrc\Host\Resources\FullControlTest . -Recurse | Out-Null
+    Pop-Location
+
+    # GacGen
+    Get-ChildItem -Path .\Tutorial -Filter Resource.xml -Recurse | %{
+        if ($_.FullName.IndexOf("\GacUI_HelloWorlds\Xml\") -ne -1) {
+            Write-Host "Compiling GacUI Resource (x86): $($_.FullName) ..."
+            $gacgen32 = Start-Process $PSScriptRoot\GacGen32.exe -ArgumentList "`"$($_.FullName)`"" -PassThru
+            $gacgen32.WaitForExit()
+        } else {
+            & $PSScriptRoot\GacGen.ps1 -FileName $_.FullName
+        }
+    }
+}
+catch {
+    Write-Host $_.Exception.Message -ForegroundColor Red
+}
+
+Pop-Location | Out-Null
+[Console]::ResetColor()
---
+++ Tools/Common.ps1
@@ -0,0 +1,47 @@
+function Build-Sln($SolutionFile, $Configuration, $Platform, $OutputVar="OutDir", $OutputFolder="") {
+    Write-Host "Building $SolutionFile ..."
+    $vsdevcmd = "$($env:VS140COMNTOOLS)VsDevCmd.bat"
+    $msbuild_arguments = "MSBUILD `"$SolutionFile`" /t:Rebuild /p:Configuration=`"$Configuration`";Platform=`"$Platform`";$OutputVar=`"$PSScriptRoot\.Output\$OutputFolder"
+    $cmd_arguments = "`"`"$vsdevcmd`" & $msbuild_arguments"
+    $wait_process = Start-Process $env:ComSpec -ArgumentList "/c $cmd_arguments" -PassThru
+    $wait_process.WaitForExit()
+}
+
+function Test-Single-Binary($FileName) {
+    if (!(Test-Path -Path .\.Output\$FileName)) {
+        throw "Failed"
+    }
+    Copy .\.Output\$FileName $FileName
+}
+
+function Test-Single-Binary-Rename($Source, $Target) {
+    if (!(Test-Path -Path .\.Output\$Source)) {
+        throw "Failed"
+    }
+    Copy .\.Output\$Source $Target
+}
+
+function Import-Project($ProjectName, [String[]]$Dependencies) {
+    Write-Host "Importing $ProjectName ..."
+    Push-Location ..\..\$ProjectName\Import | Out-Null
+    foreach ($dep in $Dependencies) {
+        Write-Host "    From $dep"
+        Copy-Item ..\..\$dep\Release\*.h .
+        Copy-Item ..\..\$dep\Release\*.cpp .
+    }
+    Pop-Location | Out-Null
+}
+
+function Release-Project($ProjectName) {
+    Write-Host "Releasing $ProjectName ..."
+    Push-Location ..\..\$ProjectName\Release | Out-Null
+    $wait_process = Start-Process "$PSScriptRoot\CodePack.exe" -ArgumentList "CodegenConfig.xml" -PassThru
+    $wait_process.WaitForExit()
+    Pop-Location | Out-Null
+}
+
+function Update-Parser($FileName) {
+    Write-Host "Updating Parser: $FileName ..."
+    $wait_process = Start-Process "$PSScriptRoot\ParserGen.exe" -ArgumentList "$FileName" -PassThru
+    $wait_process.WaitForExit()
+}
