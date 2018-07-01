3294ebf1cdd78a6be5a38d0a63b1807c3e4b56ec
Zihan Chen <vczh@163.com>
2018-07-01 16:07:41 -0700

    ...

--- Tools/GacCommon.ps1
+++ Tools/GacCommon.ps1
@@ -1,8 +1,7 @@
-function SelectXml([Xml] $xml, [String] $path) {
-    $nodes = Select-Xml -Xml $xml -XPath $path
+function ForceArray($nodes) {
     if ($nodes -eq $null) {
         return ,@()
-    } elseif ($nodes -is [array]) {
+    } elseif ($nodes -is [System.Array]) {
         return $nodes
     } else {
         return @($nodes)
@@ -13,7 +12,7 @@ function EnumerateResourceFiles([String] $FileName) {
     Write-Host "Searching for all resource files ..."
 
     [Xml]$gacui_xml = Get-Content $FileName
-    $excludes = (SelectXml $gacui_xml "//GacUI/Exclude/@Pattern").Node.Value
+    $excludes = (ForceArray (Select-Xml -Xml $gacui_xml -XPath "//GacUI/Exclude/@Pattern")).Node.Value
     $search_directory = Split-Path -Parent (Resolve-Path $FileName)
     $resource_files = (Get-ChildItem $search_directory -Filter "*.xml" -Recurse | ForEach-Object {
         $normalized_path = $_.FullName -replace '\\','/'
@@ -47,23 +46,19 @@ function DumpResourceFiles([String] $FileName, [HashTable]$ResourceDumpFiles) {
 }
 
 function NeedBuild([Xml] $Dump) {
-    $input_files = (SelectXml $Dump "//ResourceMetadata/Inputs/Input/@Path").Node.Value
-    $output_files = (SelectXml $Dump "//ResourceMetadata/Outputs/Output/@Path").Node.Value
+    $input_files = (ForceArray (Select-Xml -Xml $Dump -XPath "//ResourceMetadata/Inputs/Input/@Path")).Node.Value
+    $output_files = (ForceArray (Select-Xml -Xml $Dump -XPath "//ResourceMetadata/Outputs/Output/@Path")).Node.Value
 
     if (($output_files | Where-Object { -not [System.IO.File]::Exists($_) }) -ne $null) {
         return $true
     }
 
-    $input_file_times = $input_files | ForEach-Object {
+    $input_file_times = ForceArray ($input_files | ForEach-Object {
         [System.IO.FileInfo]::new($_).LastWriteTimeUtc
-    }
-    $output_file_times = $output_files | ForEach-Object {
+    })
+    $output_file_times = ForceArray ($output_files | ForEach-Object {
         [System.IO.FileInfo]::new($_).LastWriteTimeUtc
-    }
-    
-    if (-not ($input_file_times -is [System.Array])) {
-        $input_file_times = @($input_file_times)
-    }
+    })
 
     $outdated = $output_file_times | Where-Object {
         $output = $_
@@ -79,24 +74,21 @@ function NeedBuild([Xml] $Dump) {
 function EnumerateBuildCandidates([HashTable] $ResourceDumps, [String] $OutputFileName) {
     Write-Host "Finding resource files that need rebuild ..."
     $build_candidates = $ResourceDumps.Keys | Where-Object { NeedBuild $ResourceDumps[$_] } | Sort-Object
-    if ($build_candidates -eq $null) { $build_candidates = @() }
-    [System.IO.File]::WriteAllLines($OutputFileName, $build_candidates)
+    [System.IO.File]::WriteAllLines($OutputFileName, (ForceArray $build_candidates))
 }
 
 function EnumerateAnonymousResources([HashTable] $ResourceDumps, [String] $OutputFileName) {
     Write-Host "Finding anonymouse resource files ..."
     $file_names = $ResourceDumps.Keys | Where-Object {
-        return (SelectXml $ResourceDumps[$_] "//ResourceMetadata/ResourceMetadata/@Name")[0].Node.Value -eq ""
+        return (ForceArray (Select-Xml -Xml $ResourceDumps[$_] -XPath "//ResourceMetadata/ResourceMetadata/@Name"))[0].Node.Value -eq ""
     } | Sort-Object
-    if ($file_names -eq $null) { $file_names = @() }
-    [System.IO.File]::WriteAllLines($OutputFileName, $file_names)
+    [System.IO.File]::WriteAllLines($OutputFileName, (ForceArray $file_names))
 }
 
 function EnumerateNamedResources([HashTable] $ResourceDumps, [String] $OutputFileName) {
     Write-Host "Finding named resource files ..."
     $file_names = $ResourceDumps.Keys | Where-Object {
-        return (SelectXml $ResourceDumps[$_] "//ResourceMetadata/ResourceMetadata/@Name")[0].Node.Value -ne ""
+        return (ForceArray (Select-Xml -Xml $ResourceDumps[$_] -XPath "//ResourceMetadata/ResourceMetadata/@Name"))[0].Node.Value -ne ""
     } | Sort-Object
-    if ($file_names -eq $null) { $file_names = @() }
-    [System.IO.File]::WriteAllLines($OutputFileName, $file_names)
+    [System.IO.File]::WriteAllLines($OutputFileName, (ForceArray $file_names))
 }
