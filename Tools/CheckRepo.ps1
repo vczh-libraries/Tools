f97af50df2e16dd94e514091de44b715612bb991
Zihan Chen <vczh@163.com>
2019-09-09 19:34:21 -0700

    CreateCheckRepo .ps1

---
+++ Tools/CheckRepo.ps1
@@ -0,0 +1,29 @@
+function RepoCheckDirty([String]$name) {
+    Write-Host "Checking repo: $name ..."
+
+    Set-Location ..\$name | Out-Null
+    $length = $(git status --porcelain).Length
+    if ($length -ne 0) {
+        throw "$name has uncommitted files."
+    }
+
+    git remote update | Out-Null
+    $commit_aheads = $(git rev-list --left-right --count origin/master...master)
+    $array = $commit_aheads -split "\s+"
+    if ($array[1] -ne "0") {
+        throw "$name has unpushed commits."
+    }
+}
+
+Push-Location $PSScriptRoot | Out-Null
+RepoCheckDirty Vlpp
+RepoCheckDirty VlppOS
+RepoCheckDirty VlppRegex
+RepoCheckDirty VlppReflection
+RepoCheckDirty VlppParser
+RepoCheckDirty Workflow
+RepoCheckDirty GacUI
+RepoCheckDirty Release
+RepoCheckDirty vczh-libraries.github.io
+Pop-Location | Out-Null
+[Console]::ResetColor()
