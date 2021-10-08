e56f5025498dc92055e9b2f02469f25b51c481df
vczh <vczh@outlook.com>
2021-10-07 22:41:55 -0700

    Update ProjectWorkflow.ps1

--- Tools/ProjectWorkflow.ps1
+++ Tools/ProjectWorkflow.ps1
@@ -1,19 +1,12 @@
-function Test-Workflow-Platform($platform, $vcxproj, $outDir, $executable) {
-    Build-Sln $vcxproj Release $platform OutDir "`"$outDir\`""
-    if (!(Test-Path "$outDir\$executable")) {
-        throw "Failed"
-    }
-
-    Write-Host "Executing Unit Test ($platform) ..."
-    Start-Process-And-Wait (,("$outDir\$executable", ""))
-}
-
 function Test-Workflow {
-    Test-Workflow-Platform Win32 $PSScriptRoot\..\..\Workflow\Test\UnitTest\UnitTest\UnitTest.vcxproj                       "$PSScriptRoot\..\..\Workflow\Test\UnitTest\Release"     "UnitTest.exe"
-    Test-Workflow-Platform x64   $PSScriptRoot\..\..\Workflow\Test\UnitTest\UnitTest\UnitTest.vcxproj                       "$PSScriptRoot\..\..\Workflow\Test\UnitTest\x64\Release" "UnitTest.exe"
-    Test-Workflow-Platform Win32 $PSScriptRoot\..\..\Workflow\Test\UnitTest\UnitTest_MergeCpp\UnitTest_MergeCpp.vcxproj     "$PSScriptRoot\..\..\Workflow\Test\UnitTest\Release"     "UnitTest_MergeCpp.exe"
-    Test-Workflow-Platform Win32 $PSScriptRoot\..\..\Workflow\Test\UnitTest\UnitTest_CppCodegen\UnitTest_CppCodegen.vcxproj "$PSScriptRoot\..\..\Workflow\Test\UnitTest\Release"     "UnitTest_CppCodegen.exe"
-    Test-Workflow-Platform x64   $PSScriptRoot\..\..\Workflow\Test\UnitTest\UnitTest_CppCodegen\UnitTest_CppCodegen.vcxproj "$PSScriptRoot\..\..\Workflow\Test\UnitTest\x64\Release" "UnitTest_CppCodegen.exe"
+    Test-Vlpp-SubProject "Workflow" "LibraryTest"
+    Test-Vlpp-SubProject "Workflow" "CompilerTest_GenerateMetadata"
+    Test-Vlpp-SubProject "Workflow" "CompilerTest_LoadAndCompile"
+    Test-Vlpp-Platform   "Workflow" "UnitTest_MergeCpp" Win32 "$PSScriptRoot\..\..\Workflow\Test\UnitTest\Release"
+    Test-Vlpp-SubProject "Workflow" "CppTest"
+    Test-Vlpp-SubProject "Workflow" "CppTest_Metaonly"
+    Test-Vlpp-SubProject "Workflow" "CppTest_Reflection"
+    Test-Vlpp-SubProject "Workflow" "RuntimeTest"
 }
 
 function Build-Workflow {
