1816a7a48ecf1eb61c5aa3b21007eb59dd31b497
vczh <vczh@outlook.com>
2021-10-16 23:13:33 -0700

    ...

--- Ubuntu/vl/init.sh
+++ Ubuntu/vl/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-export VCPROOT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../../.."
+export VCPROOT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
+export VROOT=${VCPROOT}/../..
