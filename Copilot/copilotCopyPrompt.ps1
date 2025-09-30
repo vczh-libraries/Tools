02680cacfaa70dc5ed0f897f83bd825047242223
vczh <vczh@outlook.com>
2025-09-30 10:25:29 -0700

    Update copilotCopyPrompt.ps1

--- Copilot/copilotCopyPrompt.ps1
+++ Copilot/copilotCopyPrompt.ps1
@@ -142,9 +142,8 @@ function GenerateProcessPrompt([string]$name, [string]$ide) {
             
             # These files need to know about testing
             # 3-execution.prompt.md
-            # 4-verifying.prompt.md
             # code.prompt.md
-            if (($file.Name -eq "3-execution.prompt.md") -or ($file.Name -eq "4-verifying.prompt.md") -or ($file.Name -eq "code.prompt.md")) {
+            if (($file.Name -eq "3-execution.prompt.md") -or ($file.Name -eq "code.prompt.md")) {
                 $file_content += "`r`n" + (Get-Content "$PSScriptRoot\specific-windows\$name.md" -Raw)
                 $file_content += "`r`n" + (Get-Content "$PSScriptRoot\prompts\$ide-common\compiling.md" -Raw)
                 $file_content += "`r`n" + (Get-Content "$PSScriptRoot\prompts\common\verifying.md" -Raw)
