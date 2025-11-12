fb2ceebf02e6970812bc3ead71470cddc9340524
vczh <vczh@outlook.com>
2025-11-11 22:09:18 -0800

    Update copilotBuild.ps1

--- Copilot/vcxitems-TaskLogs/copilotBuild.ps1
+++ Copilot/vcxitems-TaskLogs/copilotBuild.ps1
@@ -26,7 +26,7 @@ if ($solutionFile -eq $null) {
 $vsdevcmd = $env:VLPP_VSDEVCMD_PATH
 if ($vsdevcmd -eq $null) {
     $MESSAGE_1 = "You have to add an environment variable named VLPP_VSDEVCMD_PATH and set its value to the path of VsDevCmd.bat, e.g.:"
-    $MESSAGE_2 = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+    $MESSAGE_2 = "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
     throw "$MESSAGE_1\r\n$MESSAGE_2"
 }
 
