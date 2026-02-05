d517bbad0a1d9a8cd27bfcd508b97d8198f0e4cc
vczh <vczh@outlook.com>
2026-02-04 20:39:48 -0800

    Begin restructure prompts for copilot

--- Copilot/copilotInit.ps1
+++ Copilot/copilotInit.ps1
@@ -6,6 +6,5 @@ Write-Host "Detected project name: $projectName"
 
 & "$PSScriptRoot\copilotCopyPrompt.ps1" -Project $projectName
 & "$PSScriptRoot\copilotCopyResource.ps1" -Project $projectName
-& "$PSScriptRoot\copilotPatchSln.ps1"
 
 Write-Host "Copilot initialization completed."
